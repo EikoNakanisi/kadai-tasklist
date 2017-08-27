b0521e7bdbd32023dd15f8a2ab1982b4bd8e5470
AkoNk <eikowind2017@gmail.com>
2017-08-27 14:31:48 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,58 +1,53 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+
   def index
-    @tasks=Task.order(created_at: :desc).page(params[:page])
   end
 
   def show
-    set_task
   end
 
   def new
-    @task=Task.new
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'タスクが正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'タスクが投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
 
   def edit
-    set_task
   end
 
   def update
-    set_task
-
-    if @task.update(task_params)
-      flash[:success] = 'タスクは正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'タスクは更新されませんでした'
-      render :edit
-    end
   end
-  
-  def destroy
-    set_task
-    @task.destroy
 
-    flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+  def destroy
+    @task.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
 
   private
+
   def task_params
     params.require(:task).permit(:content, :status)
   end
-  def set_task
-    @task=Task.find(params[:id])
-  end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+      
+      
 end
+
